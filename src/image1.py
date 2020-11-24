39ca29a71f99d95e570a0e3d2c973432ed76d7ad
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2020-11-24 20:21:19 +0000

    Add joint sinusoidal control signals

--- src/image1.py
+++ src/image1.py
@@ -23,6 +23,14 @@ class image_converter:
     self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw",Image,self.callback1)
     # initialize the bridge between openCV and ROS
     self.bridge = CvBridge()
+    self.rate = rospy.Rate(30)
+
+    # initialize a publisher to send joints' angular position to the robot
+    self.robot_joint2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
+    self.robot_joint3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
+    self.robot_joint4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
+
+    self.move()
 
 
   # Recieve data from camera 1, process it, and publish
@@ -43,6 +51,30 @@ class image_converter:
       self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
     except CvBridgeError as e:
       print(e)
+
+  # Publish data
+  def move(self):
+
+    t0 = rospy.get_time()
+
+    while not rospy.is_shutdown():
+      cur_time = np.array([rospy.get_time()])-t0
+      angle2 = np.pi/2 * np.sin(cur_time * np.pi/15)
+      angle3 = np.pi/2 * np.sin(cur_time * np.pi/18)
+      angle4 = np.pi/2 * np.sin(cur_time * np.pi/20)
+
+      joint2 = Float64()
+      joint2.data = angle2
+      joint3 = Float64()
+      joint3.data = angle3
+      joint4 = Float64()
+      joint4.data = angle4
+
+      self.robot_joint2_pub.publish(joint2)
+      self.robot_joint3_pub.publish(joint3)
+      self.robot_joint4_pub.publish(joint4)
+
+      self.rate.sleep()
 
 # call the class
 def main(args):
